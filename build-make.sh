c9c83bf9d93de593d598f1dca8ecf112b0daa3de
Re <reaperofblack@gmail.com>
2026-09-07 19:05:20 +0800

    build-make: Switch to git clone instead of downloading tarball

--- build-make.sh
+++ build-make.sh
@@ -20,8 +20,6 @@ set -e
 
 unset HOST
 
-: ${MAKE_VERSION:=4.4.1}
-
 while [ $# -gt 0 ]; do
     case "$1" in
     --host=*)
@@ -45,30 +43,11 @@ PREFIX="$(cd "$PREFIX" && pwd)"
 : ${CORES:=$(sysctl -n hw.ncpu 2>/dev/null)}
 : ${CORES:=4}
 
-download() {
-    if command -v curl >/dev/null; then
-        curl --retry 3 --retry-delay 5 --retry-all-errors -fSLO "$1"
-    else
-        wget -t 3 -w 5 "$1"
-    fi
-}
-
-if [ ! -d make-$MAKE_VERSION ]; then
-    if [ ! -e make-$MAKE_VERSION.tar.gz ]; then
-        echo "Downloading make-$MAKE_VERSION.tar.gz ..."
-        download https://ftpmirror.gnu.org/gnu/make/make-$MAKE_VERSION.tar.gz
-
-        if [ $? -ne 0 ]; then
-            echo "Error: Download make-$MAKE_VERSION.tar.gz failed."
-            exit 1
-        fi
-    fi
-
-    echo "Extracting make-$MAKE_VERSION.tar.gz ..."
-    tar -zxf make-$MAKE_VERSION.tar.gz
+if [ ! -d make ]; then
+    git clone --depth 1 https://github.com/mirror/make.git
 fi
 
-cd make-$MAKE_VERSION
+cd make
 
 if [ -n "$HOST" ]; then
     CONFIGFLAGS="$CONFIGFLAGS --host=$HOST"
