19a6e89307c846ffb735e7f626b735025d6a6b86
Re <43136644+real-re@users.noreply.github.com>
2026-09-08 15:10:59 +0000

    build-make: Fix build

--- build-make.sh
+++ build-make.sh
@@ -43,11 +43,26 @@ PREFIX="$(cd "$PREFIX" && pwd)"
 : ${CORES:=$(sysctl -n hw.ncpu 2>/dev/null)}
 : ${CORES:=4}
 
+if [ -n "$HOST" ]; then
+    case $HOST in
+    *-mingw32)
+        TARGET_WINDOWS=1
+        ;;
+    esac
+else
+    case $(uname) in
+    MINGW*)
+        TARGET_WINDOWS=1
+        ;;
+    esac
+fi
+
 if [ ! -d make ]; then
     git clone --depth 1 https://github.com/mirror/make.git
 fi
 
 cd make
+./bootstrap
 
 if [ -n "$HOST" ]; then
     CONFIGFLAGS="$CONFIGFLAGS --host=$HOST"
@@ -57,7 +72,27 @@ fi
 [ -z "$CLEAN" ] || rm -rf build$CROSS_NAME
 mkdir -p build$CROSS_NAME
 cd build$CROSS_NAME
-../configure --prefix="$PREFIX" $CONFIGFLAGS --program-prefix=mingw32- --enable-job-server LDFLAGS="-Wl,-s"
+
+LDFLAGS="-flto -ffunction-sections -fdata-sections -fno-unwind-tables"
+if [ "$(uname)" = "Darwin" ]; then
+    LDFLAGS="$LDFLAGS -Wl,-dead_strip -Wl,-dead_strip_dylibs"
+else
+    LDFLAGS="$LDFLAGS -Wl,-s -Wl,--gc-sections"
+fi
+
+# check mold linker on Linux
+if [ "$(uname)" = "Linux" ] && [ -z "$TARGET_WINDOWS" ]; then
+    if command -v mold >/dev/null; then
+        LDFLAGS="$LDFLAGS -fuse-ld=mold"
+    fi
+fi
+
+../configure --prefix="$PREFIX" $CONFIGFLAGS \
+    --program-prefix=mingw32- \
+    --enable-job-server \
+    CFLAGS="-O2" \
+    LDFLAGS="$LDFLAGS"
+
 make -j$CORES
 make install-binPROGRAMS
 mkdir -p "$PREFIX/share/make"
